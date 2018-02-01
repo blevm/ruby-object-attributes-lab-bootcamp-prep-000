b97890053728e5d3c8217494608f5db5d9ef0315
Michelle B <mjblevins7@gmail.com>
2018-02-01 07:09:18 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,11 +1,19 @@
 class Dog
 
   def name=(dog_name)
-    @this_dog_name = dog_name
+    @name = dog_name
   end
 
   def name
-    @this_dog_name
+    @name
+  end
+
+  def breed=(breed_type)
+    @breed = breed_type
+  end
+
+  def breed
+    @breed
   end
 
 end
